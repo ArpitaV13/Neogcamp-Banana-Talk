699b6b663fdb5c1c35d7eecd764acc977059c3cd
Arpita1899 <arpitaverma8885@gmail.com>
2021-06-13 23:34:37 +0530

    added the api

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 var button=document.querySelector('#button');
 var txt=document.querySelector('#txtInput');
+var outTxt=document.querySelector('#output');
+var serverUrl="https://api.funtranslations.com/translate/minion.json"
 
 
+function errorHandler(error){
+    console.log("Error ocurred",error)
+    alert("Something wrong with server, Try again after sometime")
+}
+
+
+function getTranslationUrl(text){
+    return serverUrl+"?"+"text="+text
+}
 
 
 
 function clickEventHandler(){
-    console.log('Clicked!');
-    console.log(txt.value);
+
+    var inputTxt=txt.value;
+    fetch(getTranslationUrl(inputTxt))
+    .then(response=>response.json())
+    .then(json=>{
+        var TranslatedText=json.contents.translated;
+        outTxt.innerText=TranslatedText;
+    })
+    .catch(errorHandler)
 };
 button.addEventListener("click",clickEventHandler)
 
